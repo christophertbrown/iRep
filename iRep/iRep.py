7e5f727f99fcbcd665ffc4d02cbe785dc63a7cd3
Chris Brown <ctb@ctb.dyn.berkeley.edu>
2017-09-29 14:18:49 -0700

    reverted change to imports due to error

--- iRep/iRep.py
+++ iRep/iRep.py
@@ -30,9 +30,8 @@ from itertools import product
 from multiprocessing import Pool
 
 # ctb
-#sys.path.append((os.path.dirname(os.path.abspath(__file__)).rsplit('/', 1)[0]))
-from iRep.mapped import get_reads as mapped
-from iRep.fasta import iterate_fasta as parse_fasta
+from mapped import get_reads as mapped
+from fasta import iterate_fasta as parse_fasta
 
 def plot_coverage(cov, trimmed, avg_cov, length, fit, iRep, uiRep, r2, kept_windows, title):
     """
