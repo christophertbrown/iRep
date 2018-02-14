0c6b38873050ec3b30ccc1097a892da419290c26
Chris Brown <ctb@ctb-mbp.local>
2018-02-13 21:21:15 -0800

    ctbBio

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
   name='iRep',
   packages = ['iRep'],
   scripts = ['iRep/iRep', 'iRep/bPTR', 'iRep/gc_skew.py', 'iRep/iRep_filter.py'],
-  version='1.1.12',
+  version='1.1.14',
   description='calculate iRep replication rates from metagenome sequencing',
   author='Chris Brown',
   author_email='ctb@berkeley.edu',
   license='MIT',
   url='https://github.com/christophertbrown/iRep',
   install_requires=['lmfit', 'numpy', 'scipy', 'pandas', \
-                    'seaborn', 'matplotlib'],
+                    'seaborn', 'matplotlib', 'ctbBio'],
   classifiers=[
           'Programming Language :: Python',
           'Programming Language :: Python :: 3'
